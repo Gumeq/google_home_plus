35d77f1871a8285aa4012f407e7b606f4b2a390f
Dobras Timothy <GumekTV@gmail.com>
2024-08-07 01:35:25 +0300

    url changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,10 @@ import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Index() {
-	return <div>Public Page</div>;
+	return (
+		<div>
+			Public Page
+			<a href="/login">login</a>
+		</div>
+	);
 }
